7ae3d62c87c922da54736f53dd2378ca5b57de32
AlifSakib <alifsakib@gmail.com>
2022-10-23 05:52:40 +0600

    addd routes and navbar

---
+++ src/routes/routes.js
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home";
+import Order from "../components/Order/Order";
+import Login from "../components/Pages/Login/Login";
+import Profile from "../components/Profile/Profile";
+import Shop from "../components/Shop/Shop";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "home",
+        element: <Home></Home>,
+      },
+      {
+        path: "shop",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "order",
+        element: <Order></Order>,
+      },
+      {
+        path: "profile",
+        element: <Profile></Profile>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Dosis: "'Dosis', sans-serif",
+        Montserrot: "'Montserrat', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
